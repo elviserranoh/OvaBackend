a5dfb5fee4dc88d2516ca3484c98d1505f8457f9
Elvis Serrano <elviserranoh@gmail.com>
2022-03-14 01:35:01 -0400

    lowercase

--- src/main/resources/db/changelog/changelog-1.0.sql
+++ src/main/resources/db/changelog/changelog-1.0.sql
@@ -4,10 +4,10 @@
 --comment tb_users creation tag: tb_users_changelog.0.1
 
 --
--- TB_USERS
+-- tb_users
 --
 
-create table TB_USERS(
+create table tb_users(
     id bigint primary key auto_increment,
     identity_document varchar(255) not null,
     first_name varchar(255) not null,
@@ -19,50 +19,50 @@ create table TB_USERS(
     image text
 );
 
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('23805156', 'Elvis', 'Serrano', 'elviserranoh@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_ADMIN');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('20346913', 'Eduardo', 'Sanchez', 'ed009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('24129908', 'Luisana', 'Maza', 'luisana009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('20575289', 'Carlos', 'Serrano', 'krlos009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('23805158', 'Ysabel', 'Serrano', 'ysaserrano@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('9980272', 'Linda', 'Henriquez', 'lindahenriquez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('5084188', 'Simon', 'Velazquez', 'velazquez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('7773838', 'Alguno', 'Mas', 'algunomas@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('1111223', 'Sergio', 'Jimenez', 'jimenez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('3333221', 'Irene', 'Serrano', 'Irene@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('5554422', 'Luiguie', 'Buffon', 'Luiguie@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
-INSERT INTO TB_USERS(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('7788283', 'Ricardo', 'Argona', 'Ricardo@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('23805156', 'Elvis', 'Serrano', 'elviserranoh@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_ADMIN');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('20346913', 'Eduardo', 'Sanchez', 'ed009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('24129908', 'Luisana', 'Maza', 'luisana009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('20575289', 'Carlos', 'Serrano', 'krlos009@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('23805158', 'Ysabel', 'Serrano', 'ysaserrano@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('9980272', 'Linda', 'Henriquez', 'lindahenriquez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('5084188', 'Simon', 'Velazquez', 'velazquez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('7773838', 'Alguno', 'Mas', 'algunomas@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('1111223', 'Sergio', 'Jimenez', 'jimenez@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('3333221', 'Irene', 'Serrano', 'Irene@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('5554422', 'Luiguie', 'Buffon', 'Luiguie@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
+INSERT INTO tb_users(identity_document, first_name, last_name, email, password, phone, rol) VALUES ('7788283', 'Ricardo', 'Argona', 'Ricardo@gmail.com', '$2a$10$c6VvM14Zt2bZmANjMthQC.mU/HxtYjBR5acvbcGjG4In4oewXYORm', '04148090961', 'ROLE_USER');
 
---rollback DROP TABLE TB_USERS
+--rollback DROP TABLE tb_users
 
 --changeset elvis.serrano:tb_ovas_changelog.0.1 context:dev,prod
 --comment tb_ovas creation tag: tb_ovas_changelog.0.1
 
 --
--- TB_OVAS
+-- tb_ovas
 --
 
-create table TB_OVAS(
+create table tb_ovas(
   id bigint primary key auto_increment,
   name varchar(255) not null,
   description varchar(255) not null,
   image text
 );
 
---rollback DROP TABLE TB_TOPICS
+--rollback DROP TABLE tb_ovas
 
 --changeset elvis.serrano:tb_topics_changelog.0.1 context:dev,prod
 --comment tb_topics creation tag: tb_topics_changelog.0.1
 
 --
--- TB_TOPICS
+-- tb_topics
 --
 
-create table TB_TOPICS(
+create table tb_topics(
                         id bigint primary key auto_increment,
                         indice int not null,
                         title varchar(255) not null,
                         objetive varchar(255) not null,
-                        ova_id bigint references TB_OVAS(id),
+                        ova_id bigint references tb_ovas(id),
                         image text
 );
 
@@ -72,89 +72,89 @@ create table TB_TOPICS(
 --comment tb_topic_contents creation tag: tb_topic_contents_changelog.0.1
 
 --
--- TB_TOPIC_CONTENTS
+-- tb_topic_contents
 --
 
-create table TB_TOPIC_CONTENTS(
+create table tb_topic_contents(
                           id bigint primary key auto_increment,
                           content varchar(255),
-                          topic_id bigint references TB_TOPICS(id)
+                          topic_id bigint references tb_topics(id)
 );
 
 --rollback DROP TABLE tb_topic_contents
 
---changeset elvis.serrano:TB_SUBJECT_MATTER_changelog.0.1 context:dev,prod
---comment TB_SUBJECT_MATTER creation tag: TB_SUBJECT_MATTER_changelog.0.1
+--changeset elvis.serrano:tb_subject_matter_changelog.0.1 context:dev,prod
+--comment tb_subject_matter creation tag: tb_subject_matter_changelog.0.1
 
 --
--- TB_SUBJECT_MATTER
+-- tb_subject_matter
 --
 
-create table TB_SUBJECT_MATTER(
+create table tb_subject_matter(
                                   id bigint primary key auto_increment,
                                   indice int not null,
                                   type varchar(255) not null,
                                   question text not null,
-                                  topic_id bigint references TB_TOPICS(id),
+                                  topic_id bigint references tb_topics(id),
                                   image text
 );
 
---rollback DROP TABLE TB_SUBJECT_MATTER
+--rollback DROP TABLE tb_subject_matter
 
---changeset elvis.serrano:TB_SUBJECT_MATTER_COLUMN_changelog.0.1 context:dev,prod
---comment TB_SUBJECT_MATTER_COLUMN creation tag: TB_SUBJECT_MATTER_COLUMN_changelog.0.1
+--changeset elvis.serrano:tb_subject_matter_column_changelog.0.1 context:dev,prod
+--comment tb_subject_matter_column creation tag: tb_subject_matter_column_changelog.0.1
 
 --
--- TB_SUBJECT_MATTER_COLUMN
+-- tb_subject_matter_column
 --
 
-create table TB_SUBJECT_MATTER_COLUMN(
+create table tb_subject_matter_column(
                                          id bigint primary key auto_increment,
                                          content varchar(255) not null,
-                                         subject_matter_id bigint references TB_SUBJECT_MATTER(id)
+                                         subject_matter_id bigint references tb_subject_matter(id)
 );
 
---rollback DROP TABLE TB_SUBJECT_MATTER_COLUMN
+--rollback DROP TABLE tb_subject_matter_column
 
---changeset elvis.serrano:TB_SUBJECT_MATTER_ROW_changelog.0.1 context:dev,prod
---comment TB_SUBJECT_MATTER_ROW creation tag: TB_SUBJECT_MATTER_ROW_changelog.0.1
+--changeset elvis.serrano:tb_subject_matter_row_changelog.0.1 context:dev,prod
+--comment tb_subject_matter_row creation tag: tb_subject_matter_row_changelog.0.1
 
 --
--- TB_SUBJECT_MATTER_ROW
+-- tb_subject_matter_row
 --
 
-create table TB_SUBJECT_MATTER_ROW(
+create table tb_subject_matter_row(
                                          id bigint primary key auto_increment,
                                          content varchar(255) not null,
-                                         subject_matter_id bigint references TB_SUBJECT_MATTER(id)
+                                         subject_matter_id bigint references tb_subject_matter(id)
 );
 
---rollback DROP TABLE TB_SUBJECT_MATTER_ROW
+--rollback DROP TABLE tb_subject_matter_row
 
---changeset elvis.serrano:TB_SUBJECT_MATTER_ANSWER_CORRECT_changelog.0.1 context:dev,prod
---comment TB_SUBJECT_MATTER_ANSWER_CORRECT creation tag: TB_SUBJECT_MATTER_ANSWER_CORRECT_changelog.0.1
+--changeset elvis.serrano:tb_subject_matter_answer_correct_changelog.0.1 context:dev,prod
+--comment tb_subject_matter_answer_correct creation tag: tb_subject_matter_answer_correct_changelog.0.1
 
 --
--- TB_SUBJECT_MATTER_ANSWER_CORRECT
+-- tb_subject_matter_answer_correct
 --
 
-create table TB_SUBJECT_MATTER_ANSWER_CORRECT(
+create table tb_subject_matter_answer_correct(
                                                  id bigint primary key auto_increment,
-                                                 subject_matter_id bigint references TB_SUBJECT_MATTER(id),
-                                                 subject_matter_row_id bigint references TB_SUBJECT_MATTER_ROW(id),
-                                                 subject_matter_column_id bigint references TB_SUBJECT_MATTER_COLUMN(id)
+                                                 subject_matter_id bigint references tb_subject_matter(id),
+                                                 subject_matter_row_id bigint references tb_subject_matter_row(id),
+                                                 subject_matter_column_id bigint references tb_subject_matter_column(id)
 );
 
---rollback DROP TABLE TB_SUBJECT_MATTER_ANSWER_CORRECT
+--rollback DROP TABLE tb_subject_matter_answer_correct
 
---changeset elvis.serrano:TB_DOCUMENTS_changelog.0.1 context:dev,prod
---comment TB_DOCUMENTS creation tag: TB_DOCUMENTS_changelog.0.1
+--changeset elvis.serrano:tb_documents_changelog.0.1 context:dev,prod
+--comment tb_documents creation tag: tb_documents_changelog.0.1
 
 --
--- TB_DOCUMENTS
+-- tb_documents
 --
 
-CREATE TABLE TB_DOCUMENTS(
+CREATE TABLE tb_documents(
                              id bigint primary key auto_increment,
                              title varchar(255) not null,
                              autor varchar(255) not null,
@@ -163,33 +163,33 @@ CREATE TABLE TB_DOCUMENTS(
                              url text,
                              image text,
                              document text,
-                             ova_id bigint references TB_OVAS(id)
+                             ova_id bigint references tb_ovas(id)
 );
 
---rollback DROP TABLE TB_DOCUMENTS
+--rollback DROP TABLE tb_documents
 
---changeset elvis.serrano:TB_FEED_changelog.0.1 context:dev,prod
---comment TB_DOCUMENTS creation tag: TB_FEED_changelog.0.1
+--changeset elvis.serrano:tb_feed_changelog.0.1 context:dev,prod
+--comment tb_feed creation tag: tb_feed_changelog.0.1
 
 --
--- TB_FEED
+-- tb_feed
 --
 
-CREATE TABLE TB_FEED(
+CREATE TABLE tb_feed(
                              id bigint primary key auto_increment,
                              description text not null
 );
 
---rollback DROP TABLE TB_EVENTS
+--rollback DROP TABLE tb_feed
 
 --changeset elvis.serrano:TB_EVENTS_changelog.0.1 context:dev,prod
---comment TB_EVENTS creation tag: TB_EVENTS_changelog.0.1
+--comment tb_events creation tag: tb_events_changelog.0.1
 
 --
--- TB_EVENTS
+-- tb_events
 --
 
-CREATE TABLE TB_EVENTS(
+CREATE TABLE tb_events(
                           id bigint primary key auto_increment,
                           description text,
                           location text not null,
@@ -197,4 +197,4 @@ CREATE TABLE TB_EVENTS(
                           time time not null
 );
 
---rollback DROP TABLE TB_EVENTS
+--rollback DROP TABLE tb_events
